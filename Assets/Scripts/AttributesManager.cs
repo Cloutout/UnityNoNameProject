0e546e5c76adf99092008cea29b423f5ea6160c0
Cloutout <75390041+Cloutout@users.noreply.github.com>
2023-05-02 16:04:23 +0300

    AttributeManager oluşturuldu

---
+++ Assets/Scripts/AttributesManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttributesManager : MonoBehaviour
+{
+    public int health;
+    public int attack;
+    public int soul;
+    public int armor;
+    public float critDamage = 1.5f;
+    public float critChance = 0.5f;
+    //TODO:enemy veya karakterin caný 0 ýn altýna inince ölüm animasyonu devreye girecek
+    public void TakeDamage(int amount)
+    {
+        health -= amount - (amount * armor/100);
+
+    }
+    public void DealDamage(GameObject target) 
+    { 
+        var atm = target.GetComponent<AttributesManager>();
+        if (atm != null)
+        {
+            float totalDamage = attack;
+            if (Random.Range(0f, 1f) < critChance)
+            {
+                totalDamage *= critDamage;
+            }
+            atm.TakeDamage((int)totalDamage);
+        }
+    }
+}
