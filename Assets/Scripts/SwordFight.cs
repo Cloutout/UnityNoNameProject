ef87295cf86c34ee42032671bd07e9fde95d73c1
Cloutout <75390041+Cloutout@users.noreply.github.com>
2023-05-03 16:46:20 +0300

    pek bişey yapılmadı

---
+++ Assets/Scripts/SwordFight.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordFight : MonoBehaviour
+{
+    private Animator anim;
+    public float cooldownTime = 2f;
+    private float nextFireTime = 0f;
+    public static int noOfClick = 0;
+    float lastClickedTime = 0f;
+    float maxComboDelay = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnClick() 
+    { 
+        lastClickedTime = Time.time;
+        noOfClick++;
+        if(noOfClick == 1)
+        {
+
+        }
+
+
+
+
+
+    }
+
+
+}
